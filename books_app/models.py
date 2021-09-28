f18bbc38f36aa4e29c9389e8138afd6a6dedd7a1
CHRIS_LEE <chrismlee87@gmail.com>
2021-09-28 13:22:07 -0700

    completed lab

--- books_app/models.py
+++ books_app/models.py
@@ -3,11 +3,15 @@ from books_app import db
 from sqlalchemy.orm import backref
 import enum
 
+
 class Audience(enum.Enum):
     CHILDREN = 1
     YOUNG_ADULT = 2
     ADULT = 3
     ALL = 4
+
+# Model Class
+
 
 class Book(db.Model):
     """Book model."""
@@ -16,20 +20,23 @@ class Book(db.Model):
     publish_date = db.Column(db.Date)
 
     # The author - Who wrote it?
-    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey(
+        'author.id'), nullable=False)
     author = db.relationship('Author', back_populates='books')
-    
+
     # The audience - Who is this book written for?
     audience = db.Column(db.Enum(Audience), default=Audience.ALL)
 
     # The genres, e.g. fiction, sci-fi, fantasy
-    genres = db.relationship('Genre', secondary='book_genre', back_populates='books')
+    genres = db.relationship(
+        'Genre', secondary='book_genre', back_populates='books')
 
     def __str__(self):
         return f'<Book: {self.title}>'
 
     def __repr__(self):
-        return f'<Book: {self.title}>'
+        return f'<Book: {self.title}, Published: {self.publish_date}, id: {self.id}>'
+
 
 class Author(db.Model):
     """Author model."""
@@ -43,11 +50,13 @@ class Author(db.Model):
     def __repr__(self):
         return f'<Author: {self.name}>'
 
+
 class Genre(db.Model):
     """Genre model."""
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False, unique=True)
-    books = db.relationship('Book', secondary='book_genre', back_populates='genres')
+    books = db.relationship(
+        'Book', secondary='book_genre', back_populates='genres')
 
     def __str__(self):
         return f'<Genre: {self.name}>'
@@ -55,7 +64,10 @@ class Genre(db.Model):
     def __repr__(self):
         return f'<Genre: {self.name}>'
 
+
 book_genre_table = db.Table('book_genre',
-    db.Column('book_id', db.Integer, db.ForeignKey('book.id')),
-    db.Column('genre_id', db.Integer, db.ForeignKey('genre.id'))
-)
+                            db.Column('book_id', db.Integer,
+                                      db.ForeignKey('book.id')),
+                            db.Column('genre_id', db.Integer,
+                                      db.ForeignKey('genre.id'))
+                            )
